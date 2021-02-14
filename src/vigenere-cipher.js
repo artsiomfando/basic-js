99f4eedb3c578c1e052a5339e8dc2ba097492c95
Artsiom Fando <37732105+artsiomfando@users.noreply.github.com>
2021-02-14 19:13:53 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,96 @@
 const CustomError = require("../extensions/custom-error");
 
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modeReverse = true) {
+    this.modeReverse = modeReverse;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    } else {
+      let encMessage = [];
+      const arrMessage = message.toUpperCase().split("");
+
+      while (
+        key.length <= arrMessage.filter((el) => alphabet.includes(el)).length
+      ) {
+        key += key;
+      }
+
+      const arrKey = key
+        .toUpperCase()
+        .split("")
+        .slice(0, arrMessage.filter((el) => alphabet.includes(el)).length);
+
+      for (let i = 0; i < arrMessage.length; i++) {
+        if (!alphabet.includes(arrMessage[i]))
+          arrKey.splice(i, 0, arrMessage[i]);
+      }
+
+      for (let i = 0; i < arrMessage.length; i++) {
+        if (alphabet.includes(arrMessage[i])) {
+          const ind =
+            alphabet.indexOf(arrMessage[i]) + alphabet.indexOf(arrKey[i]) < 26
+              ? alphabet.indexOf(arrMessage[i]) + alphabet.indexOf(arrKey[i])
+              : alphabet.indexOf(arrMessage[i]) +
+                alphabet.indexOf(arrKey[i]) -
+                26;
+
+          encMessage.push(alphabet[ind]);
+        } else {
+          encMessage.push(arrMessage[i]);
+        }
+      }
+      return this.modeReverse
+        ? encMessage.join("")
+        : encMessage.reverse().join("");
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error();
+    } else {
+      let decMessage = [];
+      const arrMessage = encryptedMessage.toUpperCase().split("");
+
+      while (
+        key.length <= arrMessage.filter((el) => alphabet.includes(el)).length
+      ) {
+        key += key;
+      }
+
+      const arrKey = key
+        .toUpperCase()
+        .split("")
+        .slice(0, arrMessage.filter((el) => alphabet.includes(el)).length);
+
+      for (let i = 0; i < arrMessage.length; i++) {
+        if (!alphabet.includes(arrMessage[i]))
+          arrKey.splice(i, 0, arrMessage[i]);
+      }
+
+      for (let i = 0; i < arrMessage.length; i++) {
+        if (alphabet.includes(arrMessage[i])) {
+          const ind =
+            alphabet.indexOf(arrMessage[i]) - alphabet.indexOf(arrKey[i]) < 0
+              ? alphabet.indexOf(arrMessage[i]) +
+                26 -
+                alphabet.indexOf(arrKey[i])
+              : alphabet.indexOf(arrMessage[i]) - alphabet.indexOf(arrKey[i]);
+
+          decMessage.push(alphabet[ind]);
+        } else {
+          decMessage.push(arrMessage[i]);
+        }
+      }
+      return this.modeReverse
+        ? decMessage.join("")
+        : decMessage.reverse().join("");
+    }
   }
 }
 
